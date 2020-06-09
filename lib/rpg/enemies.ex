a51efe798bb256c83131be7eb449e69331299897
Luiz Cezer <lccezinha@gmail.com>
2020-06-08 23:44:17 -0300

    fix

--- lib/rpg/enemies.ex
+++ lib/rpg/enemies.ex
@@ -6,25 +6,25 @@ defmodule RPG.Enemies do
       %Character{
         name: "Ogre",
         description: "A large creature. Big muscles. Angry and hungry.",
-        hit_points: 12,
-        max_hit_points: 12,
-        damage_range: 3..5,
+        hit_points: 150,
+        max_hit_points: 180,
+        damage_range: 13..15,
         attack_description: "a hammer"
       },
       %Character{
         name: "Orc",
         description: "A green evil creature. Wears armor and an axe.",
-        hit_points: 8,
-        max_hit_points: 8,
-        damage_range: 2..4,
+        hit_points: 88,
+        max_hit_points: 92,
+        damage_range: 12..14,
         attack_description: "an axe"
       },
       %Character{
         name: "Goblin",
         description: "A small green creature. Wears dirty clothes and a dagger.",
-        hit_points: 4,
-        max_hit_points: 4,
-        damage_range: 1..2,
+        hit_points: 54,
+        max_hit_points: 84,
+        damage_range: 21..32,
         attack_description: "a dagger"
       }
     ]
--- lib/rpg/room/triggers/enemy.ex
+++ lib/rpg/room/triggers/enemy.ex
@@ -4,9 +4,11 @@ defmodule RPG.Room.Triggers.Enemy do
 
   def run(character, %RPG.Room.Action{id: :forward}) do
     enemy = Enum.random(RPG.Enemies.all())
+
     Shell.info(enemy.description)
     Shell.info("The enemy #{enemy.name} wants to fight.")
     Shell.info("You were prepared and attack first.")
+
     {updated_char, _enemy} = RPG.Battle.fight(character, enemy)
     {updated_char, :forward}
   end
